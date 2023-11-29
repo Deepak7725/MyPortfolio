9b13f2490066fe67da8a4e32ea824e76342464fe
Deepak7725 <125559942+Deepak7725@users.noreply.github.com>
2023-11-29 11:29:04 +0530

    added button functions

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".navbar-nav .nav-item").on("click", function () {
+        $(".navbar-nav .nav-item").removeClass("active");
+    });
+});
+function openAgencyLink() {
+    window.open('https://deepak7725.github.io/Design_Agency/', '_blank');
+}
+function openFFLink() {
+    window.open('https://deepak7725.github.io/FineFit/', '_blank');
+}
+function openNexxLink() {
+    window.open('https://deepak7725.github.io/NexxSample/', '_blank');
+}
+function openAgencyCode() {
+    window.open('https://github.com/Deepak7725/Design_Agency', '_blank');
+}
+function openFFCode() {
+    window.open('https://github.com/Deepak7725/FineFit', '_blank');
+}
+function openNexxCode() {
+    window.open('https://github.com/Deepak7725/NexxSample', '_blank');
+}
+
+
