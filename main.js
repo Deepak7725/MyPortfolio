3c3ab14649d78f5053f07b4fd1310efe3d5fe25d
Deepak7725 <125559942+Deepak7725@users.noreply.github.com>
2023-12-19 21:40:25 +0530

    update index and main js

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $(".navbar-nav .nav-item").on("click", function () {
+        $(".navbar-nav .nav-item").removeClass("active");
+    });
+});
+function openAgencyLink() {
+    window.open('https://deepak7725.github.io/Design_Agency/', '_blank');
+}
+function openFFLink() {
+    window.open('https://deepak7725.github.io/FineFit/', '_blank');
+}
+function openNexxLink() {
+    window.open('https://deepak7725.github.io/NexxSample/', '_blank');
+}
+function openDroneLink() {
+    window.open('https://deepak7725.github.io/Drone/', '_blank');
+}
+function openSapphireLink() {
+    window.open('https://deepak7725.github.io/Sapphire/', '_blank');
+}
+
+function openAgencyCode() {
+    window.open('https://github.com/Deepak7725/Design_Agency', '_blank');
+}
+function openFFCode() {
+    window.open('https://github.com/Deepak7725/FineFit', '_blank');
+}
+function openNexxCode() {
+    window.open('https://github.com/Deepak7725/NexxSample', '_blank');
+}
+function openDroneCode() {
+    window.open('https://github.com/Deepak7725/Drone', '_blank');
+}
+function openSapphireCode() {
+    window.open('https://github.com/Deepak7725/Sapphire', '_blank');
+}
+
+
+
+
